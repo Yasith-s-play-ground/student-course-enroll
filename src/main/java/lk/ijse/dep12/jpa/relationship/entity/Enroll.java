de6b68f37e07a8fa0e8e9bfec06a7ea13e19d391
Yasith Ishara Perera <yasithishara@gmail.com>
2024-07-23 14:37:27 +0530

    New Method IdClass

--- src/main/java/lk/ijse/dep12/jpa/relationship/entity/Enroll.java
+++ src/main/java/lk/ijse/dep12/jpa/relationship/entity/Enroll.java
@@ -13,40 +13,19 @@ import java.sql.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "enroll")
+@IdClass(EnrollPK.class)
 public class Enroll implements Serializable {
-    @EmbeddedId
-    private EnrollPK enrollPK;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "student_id",referencedColumnName = "id")
+    private Student student;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "course_code",referencedColumnName = "code")
+    private Course course;
     @Column(name = "registered_by")
     private String registeredBy;
     private Date date;
 
-    @ManyToOne
-//    @JoinColumn(name = "student_id", referencedColumnName = "id", insertable = false, updatable = false)
-    @MapsId("studentId")
-    private Student student;
 
-    @ManyToOne
-//    @JoinColumn(name = "course_code", referencedColumnName = "code", insertable = false, updatable = false)
-    @MapsId("courseCode")
-    private Course course;
-
-    public Enroll(EnrollPK enrollPK, String registeredBy, Date date) {
-        this.enrollPK = enrollPK;
-        this.registeredBy = registeredBy;
-        this.date = date;
-    }
-
-    public Enroll(String studentId, String courseCode, String registeredBy, Date date) {
-        this.enrollPK = new EnrollPK(studentId, courseCode);
-        this.registeredBy = registeredBy;
-        this.date = date;
-    }
-
-    public Enroll(Student student, Course course, String registeredBy, Date date) {
-        this.enrollPK = new EnrollPK(student.getId(), course.getCode());
-        this.registeredBy = registeredBy;
-        this.date = date;
-        this.course = course;
-        this.student = student;
-    }
 }
--- src/main/java/lk/ijse/dep12/jpa/relationship/entity/EnrollPK.java
+++ src/main/java/lk/ijse/dep12/jpa/relationship/entity/EnrollPK.java
@@ -11,10 +11,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Embeddable
 public class EnrollPK implements Serializable {
-    @Column(name = "student_id")
-    private String studentId;
-    @Column(name = "course_code")
-    private String courseCode;
+    private Student student;
+    private Course course;
 }
